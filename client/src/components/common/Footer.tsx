f9a06a29c63365880677c69ad2019b0255302876
gade9096 <gadedharmraj@gmail.com>
2025-07-12 14:09:44 +0530

    Add files via upload

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/pratiknikam9096"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Pratik Nikam
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
